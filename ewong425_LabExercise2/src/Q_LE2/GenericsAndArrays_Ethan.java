4eea2da1275c748c3a11ab86489623f7058a4d1a
Ethan Wong <ewong425@uwo.ca>
2022-09-27 23:08:33 -0400

    Finished GenericsAndArrays

--- ewong425_LabExercise2/src/Q_LE2/GenericsAndArrays_Ethan.java
+++ ewong425_LabExercise2/src/Q_LE2/GenericsAndArrays_Ethan.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class GenericsAndArrays_Ethan {
     public static void main(String[] args) {
-        int input=0;
+        int input=0; String cont = "y";
     header(2);
     ArrayList<Integer> yearOfStudies = new ArrayList<>(Arrays.asList(2,3,4,3,2,2));
     ArrayList<String> nameOfStudents = new ArrayList<>(Arrays.asList("Harry", "Lavender", "Ron", "Hermione", "Luna", "Vincent"));
@@ -16,25 +16,28 @@ public class GenericsAndArrays_Ethan {
     Scanner sc = new Scanner(System.in);
         System.out.println("This program prints the names of the students leaders from year 2,3 and 4. \n" +
                             "To see the list of students from a specific year enter an integer between 2 and 4 when prompted.");
-        while(true) {
-            System.out.println("Enter Academic Year (2,3 or 4):");
-            input = sc.nextInt();
-            try {
-                if (input <= 4 && input >= 2) {
-                    break;
+        while(cont.equals("y")) {
+            while (true) {
+                System.out.println("Enter Academic Year (2,3 or 4):");
+                input = sc.nextInt();
+                try {
+                    if (input <= 4 && input >= 2) {
+                        break;
+                    }
+                } catch (Exception e) {
+                    System.out.print("Incorrect Input! ");
                 }
-            } catch (Exception e) {
-                System.out.print("Incorrect Input! ");
             }
+            ArrayList<Object> returnArray = new ArrayList<>();
+            for (int i = 0; i < pair.size(); i++) {
+                if (pair.get(i).getKey().equals(input)) {
+                    returnArray.add(pair.get(i).getValue());
+                }
+            }
+            System.out.println("Do you wish to continue? (Press y to continue or any other key to terminate.");
+            cont = sc.nextLine();
         }
-        ArrayList<Object> returnArray = new ArrayList<>();
-    for(int i=0; i<pair.size(); i++) {
-        if(pair.get(i).getKey().equals(input)) {
-            returnArray.add(pair.get(i).getValue());
-        }
-    }
-
-    footer(2);
+        footer(2);
     }
     public static void header(int n) {
         System.out.println("Lab Exercise: " + n + "-Q1");
